92a5aa0464dd5e417f965aeb90f08548143bf63b
Eric Charles <eric@datalayer.io>
2025-08-11 08:11:56 +0200

    bump: 0.2.0

--- jupyter_mimetypes/__init__.py
+++ jupyter_mimetypes/__init__.py
@@ -8,7 +8,7 @@ Dynamically inject new Jupyter representation to some objects.
 
 from jupyter_mimetypes.api import deserialize_object, get_variable, serialize_object, set_variable
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "__version__",
     "deserialize_object",
